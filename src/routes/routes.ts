f4aab0dbfe2fa773f9a21ee4ea27c1a52cd26c60
Jose Angel Montalvo <jose.montalvo@conversia.es>
2024-09-19 13:23:59 +0200

    Lazyload basico listoç

---
+++ src/routes/routes.ts
@@ -0,0 +1,33 @@
+import { lazy, LazyExoticComponent } from "react";
+
+type JSXComponent = () =>  JSX.Element
+
+interface Route {
+  path: string;
+  Component: LazyExoticComponent<JSXComponent> | JSXComponent;
+  name: string,
+  children?: Route[]
+}
+
+const Lazy1 = lazy(() => import(/*webpackChunkName: "LazyPage1"*/'../01-lazyload/pages/LazyPage1'));
+const Lazy2 = lazy(() => import(/*webpackChunkName: "LazyPage2"*/'../01-lazyload/pages/LazyPage2'));
+const Lazy3 = lazy(() => import(/*webpackChunkName: "LazyPage3"*/'../01-lazyload/pages/LazyPage3'));
+
+export const routes: Route[] = [
+  {
+    path: "/lazy1",
+    Component: Lazy1,
+    name: "LazyPage1",
+    children: []
+  },
+  {
+    path: "/lazy2",
+    Component: Lazy2,
+    name: "LazyPage2",
+  },
+  {
+    path: "/lazy3",
+    Component: Lazy3,
+    name: "LazyPage3",
+  },
+];
